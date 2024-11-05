58179183c24cc201f8fc9a952916e3a53b8238d5
Vishal Yadav <Vishal.yadav2279@gmail.com>
2024-11-05 16:47:26 +0530

    Updated metrics controller

--- app/controllers/metrics_controller.rb
+++ app/controllers/metrics_controller.rb
@@ -1,8 +1,17 @@
 # frozen_string_literal: true
 
+require 'prometheus/client'
+require 'prometheus/client/formats/text'
+
 class MetricsController < ActionController::Base
   def index
     prometheus = Prometheus::Client.registry
-    render plain: prometheus.text, content_type: "text/plain"
+    # Serialize metrics in Prometheus text format
+    metrics_text = Prometheus::Client::Formats::Text.marshal(prometheus)
+
+    render plain: metrics_text, content_type: 'text/plain'
+  rescue => e
+    Rails.logger.error("Error generating metrics: #{e.message}")
+    render plain: "Error generating metrics", status: :internal_server_error
   end
 end
